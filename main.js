8d9cbfdb00220e068b9abb3326170ccb60846841
ktldsm27 <122687754+ktldsm27@users.noreply.github.com>
2023-09-09 17:16:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ function displaySum() {
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
+
+// id attribute has been set in all three
+
+<span id= "firstNum">2</span> <br> 
+    ...<span id= "secondNum">7</span> 
+    ...... <span id= "answer">(placeholder for the answer)</span>
+
+//getElementById will get all HTML elements by a specific "id" attribute
+...
+let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum + secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
